6c9da60ca24093c088f0e88f3f22288b10fb83a2
Tetra <jros512@wgu.edu>
2023-11-02 10:23:24 -0500

    before attempting to read and write from external files

--- sub.py
+++ sub.py
@@ -11,8 +11,26 @@ class SubWindow(QMainWindow):
     def __init__(self, main_ui):
         super(SubWindow, self).__init__()
         loadUi("sub.ui", self)
-        self.pushButton.clicked.connect(self.close)
+
+        self.close_button.clicked.connect(self.close)
         self.main_ui = main_ui
+
+        self.spinBox_1_value = 0
+        self.spinBox_2_value = 0
+
+        self.spinBox.valueChanged.connect(self.spinBox_on_changed)
+        self.spinBox_2.valueChanged.connect(self.spinBox_2_on_changed)
+        self.total_button.clicked.connect(self.total)
+
+    def spinBox_on_changed(self, value):
+        self.spinBox_1_value = value
+
+    def spinBox_2_on_changed(self, value):
+        self.spinBox_2_value = value
+
+    def total(self):
+        total = self.spinBox_1_value + self.spinBox_2_value
+        self.label.setText(str(total))
 
     def closeEvent(self, event):
         self.closed.emit()
