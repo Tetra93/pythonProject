70f5d564e6016db09a10a9ad0046693820f114f8
Tetra <jros512@wgu.edu>
2023-11-06 11:47:59 -0600

    added races with stats

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 import json
 
 from PyQt6.QtWidgets import (QMainWindow, QApplication, QPushButton,
-                             QLabel, QScrollArea, QCheckBox, QWidget, QGridLayout)
+                             QLabel, QScrollArea, QCheckBox, QWidget, QGridLayout, QComboBox)
 from PyQt6.uic import loadUi
 from PyQt6.QtCore import pyqtSignal
 
@@ -18,6 +18,7 @@ class MainUI(QMainWindow):
 
     sub_button: QPushButton
     calculate_button: QPushButton
+    comboBox: QComboBox
     label1: QLabel
     label2: QLabel
     label3: QLabel
@@ -27,14 +28,21 @@ class MainUI(QMainWindow):
     label1_text = 'Strength'
     label2_text = 'Defense'
     label3_text = 'Magic'
-    label1_2_text = 0
-    label2_2_text = 0
-    label3_2_text = 0
+    artifact_strength = 0
+    artifact_defense = 0
+    artifact_magic = 0
+    race_strength = 0
+    race_defense = 0
+    race_magic = 0
+    equip_strength = 0
+    equip_defense = 0
 
     def __init__(self):
         super(MainUI, self).__init__()
         loadUi("mainWindow.ui", self)
 
+        self.comboBox.addItems(['Clavat', 'Selkie', 'Lilty', 'Yuke'])
+        self.comboBox.currentTextChanged.connect(self.race_changed)
         self.widget = QWidget(self.centralwidget)
         self.widget.setGeometry(60, 10, 330, 180)
         self.grid = QGridLayout(self.widget)
@@ -55,16 +63,11 @@ class MainUI(QMainWindow):
             self.grid.addWidget(checkbox, row, 3)
             row += 1
 
-        #self.widget.show()
-        #self.scroll_area.show()
         self.sub_button.clicked.connect(self.sub_on_click)
-        #self.calculate_button.clicked.connect(self.calculate_on_click)
         self.label1.setText(self.label1_text)
         self.label2.setText(self.label2_text)
         self.label3.setText(self.label3_text)
-        self.label1_2.setText(str(self.label1_2_text))
-        self.label2_2.setText(str(self.label2_2_text))
-        self.label3_2.setText(str(self.label3_2_text))
+        self.race_changed()
         self.sub_ui = None
 
     def sub_on_click(self):
@@ -73,24 +76,51 @@ class MainUI(QMainWindow):
         self.sub_ui.show()
         self.sub_ui.closed.connect(self.show_main_ui)
 
+    def race_changed(self):
+        race = self.comboBox.currentText()
+        if race == 'Clavat':
+            self.race_strength = 8
+            self.race_defense = 7
+            self.race_magic = 13
+        elif race == 'Selkie':
+            self.race_strength = 7
+            self.race_defense = 6
+            self.race_magic = 12
+        elif race == 'Lilty':
+            self.race_strength = 8
+            self.race_defense = 8
+            self.race_magic = 10
+        elif race == 'Yuke':
+            self.race_strength = 5
+            self.race_defense = 5
+            self.race_magic = 15
+
+        self.total_stats()
+
     def on_checkbox_changed(self, state, stat, value):
         if state:
             if stat == 'Strength':
-                self.label1_2_text += value
+                self.artifact_strength += value
             elif stat == 'Defense':
-                self.label2_2_text += value
+                self.artifact_defense += value
             elif stat == 'Magic':
-                self.label3_2_text += value
+                self.artifact_magic += value
         else:
             if stat == 'Strength':
-                self.label1_2_text -= value
+                self.artifact_strength -= value
             elif stat == 'Defense':
-                self.label2_2_text -= value
+                self.artifact_defense -= value
             elif stat == 'Magic':
-                self.label3_2_text -= value
-        self.label1_2.setText(str(self.label1_2_text))
-        self.label2_2.setText(str(self.label2_2_text))
-        self.label3_2.setText(str(self.label3_2_text))
+                self.artifact_magic -= value
+        self.total_stats()
+        
+    def total_stats(self):
+        strength = (self.artifact_strength + self.race_strength + self.equip_strength)
+        defense = (self.artifact_defense + self.race_defense + self.equip_defense)
+        magic = (self.artifact_magic + self.race_magic)
+        self.label1_2.setText(str(strength))
+        self.label2_2.setText(str(defense))
+        self.label3_2.setText(str(magic))
 
     def show_main_ui(self):
         self.show()
